462111a820112327d23b240ba90d448c2fbca006
THE-LEVITE <halfonsokyar03@gmail.com>
2025-12-11 20:03:04 -0500

    v_1.0-CreacionBaseProyecto_the-levite_11122025

---
+++ src/main/java/com/repq/demo/SistemaRepqApplication.java
@@ -0,0 +1,13 @@
+package com.repq.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaRepqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaRepqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/repq/demo/SistemaRepqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.repq.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaRepqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
