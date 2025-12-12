7c75bf44bcb5c23a486ea48370f9adf2a7f5a9d2
THE-LEVITE <halfonsokyar03@gmail.com>
2025-12-11 21:06:18 -0500

    v_1.2-PrimerosPasos_the-levite_11122025

--- src/main/java/com/repq/entidad/Administrador.java
+++ src/main/java/com/repq/entidad/Administrador.java
@@ -1,10 +1,24 @@
 package com.repq.entidad;
 
+import java.sql.Date;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table (name = "administradores")
 public class Administrador {
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //Anotacion JASON
+	private Integer id;
+	private String nombre;
+	private String usuario;
+	private String password;
+	private String email;
+	private Date fecha_creacion;
+
+
 }
